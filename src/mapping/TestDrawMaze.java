09b7f96ae05101108ababd03d60699b63b117196
Jon Bell <jab414@cca-lg36-009.cs.bham.ac.uk>
2019-03-07 21:11:56 +0000

    Testing Class for drawing maze

---
+++ src/mapping/TestDrawMaze.java
@@ -0,0 +1,15 @@
+package mapping;
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.ev3.EV3;
+
+public class TestDrawMaze {
+
+	public static void main(String args[]) {
+		EV3 ev3brick = (EV3) BrickFinder.getLocal();
+		Maze testMaze = new Maze();
+		DrawMaze draw =  new DrawMaze();
+		DrawMaze.drawMaze(testMaze);
+		ev3brick.getKeys().waitForAnyPress();
+	}
+}
